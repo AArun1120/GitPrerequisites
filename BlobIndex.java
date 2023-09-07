af85eb29136c0a365c8a4cd3085ad8387052af04
Aaron Moradi <adaar2005@gmail.com>
2023-09-07 12:20:03 -0700

    Added Sha1Hashing method to blobIndex and added sha1tester to check function.

---
+++ BlobIndex.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class BlobIndex {
+
+    public static String sha1Hash(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
